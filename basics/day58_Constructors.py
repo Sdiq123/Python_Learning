d8c57cf5e617bfd00001ef0a5d53541e7879d89d
Sadiq <sadiqarbazpasha@gmail.com>
2026-05-16 10:30:56 +0530

    comitting till 58

--- basics/day58_Constructors.py
+++ basics/day58_Constructors.py
@@ -5,7 +5,7 @@ Constructor ek object bananeke liye madad karta hein
 
 kabhi kabhi hamlog arguments pass karkey object banana chahtey hein, to aisey mein constructors hamey initialization karney mein madad kartey hein
 
-__init__ - Method
+__init__ - Method , hamesha invoke hoga jab bhi ham ek naya object banayengey vo class ka
 
 '''
 
@@ -21,15 +21,24 @@ __init__ - Method
 
 
 class Person:
-    def __init__(self):     # ye ek special method hein jo hamey constructor bananey ke liye madad karta hein
+    def __init__(self,name,occ):     # ye ek special method hein jo hamey constructor bananey ke liye madad karta hein
+        print("Hey I am a person")
         self.name = name
+        self.occ = occ
     
     def info(self): # koi bhi method aap likhogey to usko self parameter chahiye inside a class
         print(f"{self.name} is  a {self.occ}")
         
         
 
-a = Person()
+a = Person("Harry", "Developer" )  # jab bhi main object banaunga to directly constructor ko call karunga  
+# jab bhi main a = Person() call karun to vo directly print karega, mujhey separately call karney ki zarurat nahi hein
+b = Person("Divya", "HR")   # idhar self as a object parameter pass ho raha hein,matlab 'self' ki jagah b pass ho rahah hein , to teen arguments pass karney ki zarurat nahi hein
+# ab jab main dusra object banaunga to bhi default __init__ method directly call hoga 
+c = Person()
+
+a.info()
+b.info()
 # print(a.name)
 
 '''
@@ -37,6 +46,14 @@ main idhar 'a.name' aur 'a.occ' mein 'divya' dena chahtha hun to main ye kaisey 
 
 '''
 
-a.name = "Divya"
-a.occ = "HR"
-a.info()
+# a.name = "Divya"
+# a.occ = "HR"
+# a.info()
+
+'''
+Constructor ka main kaam hein value ko initialize karna aur vo hamesha None return karega
+Two types of Constructors 
+1. Parameterized Constructor 
+2. Default Constructor - jo arguments nahi leta aur sirf self leta hein
+
+'''
